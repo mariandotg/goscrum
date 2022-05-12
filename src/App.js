2e0240837a28659c37e99f67a4c60ff68d794b44
mariandotg <marianguillaume.m@gmail.com>
2022-05-12 18:41:18 -0300

    Added login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 const App = () => {
-  return <div>goscrum</div>;
+  return (
+    <div className="container">
+      <form>
+        <h1>Iniciar sesión</h1>
+        <div>
+          <label>Email</label>
+          <input name="email" type="email" />
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default App;
