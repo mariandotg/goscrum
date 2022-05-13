9856253ce344cbeec7e19b407fef9420e7563051
mariandotg <marianguillaume.m@gmail.com>
2022-05-13 12:54:18 -0300

    Fixed paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/views/Login/Login";
+import Register from "./components/views/Register/Register";
+import Error404 from "./components/views/Error 404/Error404";
+import Tasks from "./components/views/Tasks/Tasks";
 import "./App.css";
-import Login from "./components/Login/Login";
 
 const App = () => {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Tasks />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
 };
 
 export default App;
