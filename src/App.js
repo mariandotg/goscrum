471e759759f51913ea93254277f611eb1eabe851
Mariano Guillaume <84543990+mariandotg@users.noreply.github.com>
2022-05-14 00:03:28 -0300

    Simple routing features (#3)
    * Added routing
    
    * Added Error404 view
    
    * Added Tasks view
    
    * Fixed paths
    
    * Added private routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/views/Login/Login";
+import Register from "./components/views/Register/Register";
+import Error404 from "./components/views/Error 404/Error404";
+import Tasks from "./components/views/Tasks/Tasks";
 import "./App.css";
-import Login from "./components/Login/Login";
+
+const RequireAuth = ({ children }) => {
+  if (!localStorage.getItem("logged")) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  return children;
+};
 
 const App = () => {
-  return <Login />;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Tasks />
+          </RequireAuth>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/components/views/Tasks/Tasks.jsx
@@ -0,0 +1,5 @@
+const Tasks = () => {
+  return <div className="container">Tasks</div>;
+};
+
+export default Tasks;
