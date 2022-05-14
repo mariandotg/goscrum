1805c03f4a71020dace5b83bc0beb8826c324d9f
mariandotg <marianguillaume.m@gmail.com>
2022-05-13 23:59:48 -0300

    Added private routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/views/Login/Login";
 import Register from "./components/views/Register/Register";
 import Error404 from "./components/views/Error 404/Error404";
 import Tasks from "./components/views/Tasks/Tasks";
 import "./App.css";
 
+const RequireAuth = ({ children }) => {
+  if (!localStorage.getItem("logged")) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  return children;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Tasks />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Tasks />
+          </RequireAuth>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="*" element={<Error404 />} />
