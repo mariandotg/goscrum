96082739497a611f67cc5a9992c08e4ed3b532d6
mariandotg <marianguillaume.m@gmail.com>
2022-05-16 15:59:08 -0300

    Added logout function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import "./Header.styles.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("logged");
+    navigate("/", { replace: true });
+  };
+
   return (
     <header>
       <img src="/img/goscrum.png" alt="logo" />
-      <div>X</div>
+      <div onClick={handleLogout}>X</div>
     </header>
   );
 };
