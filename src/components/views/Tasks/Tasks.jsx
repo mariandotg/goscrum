6ae34ef8e0cf55787622bb169f456861ccdfeae5
mariandotg <marianguillaume.m@gmail.com>
2022-05-17 16:47:01 -0300

    Added List for phone and desktop

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -10,7 +10,63 @@ const Tasks = () => {
           <div className="list_header">
             <h2>Mis tareas</h2>
           </div>
-          <div className="list">
+          <div className="list_group">
+            <div className="list">
+              <h4>Nuevas</h4>
+              <div className="card">
+                <div className="close">X</div>
+                <h3>Tarea 1</h3>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Julio Avantt</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción</p>
+              </div>
+            </div>
+            <div className="list">
+              <h4>En proceso</h4>
+              <div className="card">
+                <div className="close">X</div>
+                <h3>Tarea 1</h3>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Julio Avantt</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción</p>
+              </div>
+            </div>
+            <div className="list">
+              <h4>Finalizadas</h4>
+              <div className="card">
+                <div className="close">X</div>
+                <h3>Tarea 1</h3>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Julio Avantt</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción</p>
+              </div>
+            </div>
+          </div>
+          <div className="list phone">
+            <div className="card">
+              <div className="close">X</div>
+              <h3>Tarea 1</h3>
+              <h6>24/1/2022 16:40hs</h6>
+              <h5>Julio Avantt</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Alta</button>
+              <p>Descripción</p>
+            </div>
+            <div className="card">
+              <div className="close">X</div>
+              <h3>Tarea 1</h3>
+              <h6>24/1/2022 16:40hs</h6>
+              <h5>Julio Avantt</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Alta</button>
+              <p>Descripción</p>
+            </div>
             <div className="card">
               <div className="close">X</div>
               <h3>Tarea 1</h3>
