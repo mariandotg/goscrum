666d53752fa3d01f89b1f9fe30f45309554bb673
mariandotg <marianguillaume.m@gmail.com>
2022-05-17 16:57:08 -0300

    Added responsiveness

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -7,6 +7,16 @@ const Tasks = () => {
     window.innerWidth < 900 ? true : false
   );
 
+  const handleResize = () => {
+    if (window.innerWidth < 900) setIsPhone(true);
+    else setIsPhone(false);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
   return (
     <>
       <Header />
