d8955805107cacd7d088af9867dbb8b0470a88d5
mariandotg <marianguillaume.m@gmail.com>
2022-05-17 16:49:57 -0300

    Added simple conditional render of List component

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from "react";
 import "./Tasks.styles.css";
 import Header from "../../Header/Header";
 
 const Tasks = () => {
+  const [isPhone, setIsPhone] = useState(
+    window.innerWidth < 900 ? true : false
+  );
+
   return (
     <>
       <Header />
@@ -10,9 +15,26 @@ const Tasks = () => {
           <div className="list_header">
             <h2>Mis tareas</h2>
           </div>
-          <div className="list_group">
-            <div className="list">
-              <h4>Nuevas</h4>
+          {isPhone ? (
+            <div className="list phone">
+              <div className="card">
+                <div className="close">X</div>
+                <h3>Tarea 1</h3>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Julio Avantt</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción</p>
+              </div>
+              <div className="card">
+                <div className="close">X</div>
+                <h3>Tarea 1</h3>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Julio Avantt</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción</p>
+              </div>
               <div className="card">
                 <div className="close">X</div>
                 <h3>Tarea 1</h3>
@@ -23,60 +45,46 @@ const Tasks = () => {
                 <p>Descripción</p>
               </div>
             </div>
-            <div className="list">
-              <h4>En proceso</h4>
-              <div className="card">
-                <div className="close">X</div>
-                <h3>Tarea 1</h3>
-                <h6>24/1/2022 16:40hs</h6>
-                <h5>Julio Avantt</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Alta</button>
-                <p>Descripción</p>
+          ) : (
+            <div className="list_group">
+              <div className="list">
+                <h4>Nuevas</h4>
+                <div className="card">
+                  <div className="close">X</div>
+                  <h3>Tarea 1</h3>
+                  <h6>24/1/2022 16:40hs</h6>
+                  <h5>Julio Avantt</h5>
+                  <button type="button">Nueva</button>
+                  <button type="button">Alta</button>
+                  <p>Descripción</p>
+                </div>
+              </div>
+              <div className="list">
+                <h4>En proceso</h4>
+                <div className="card">
+                  <div className="close">X</div>
+                  <h3>Tarea 1</h3>
+                  <h6>24/1/2022 16:40hs</h6>
+                  <h5>Julio Avantt</h5>
+                  <button type="button">Nueva</button>
+                  <button type="button">Alta</button>
+                  <p>Descripción</p>
+                </div>
+              </div>
+              <div className="list">
+                <h4>Finalizadas</h4>
+                <div className="card">
+                  <div className="close">X</div>
+                  <h3>Tarea 1</h3>
+                  <h6>24/1/2022 16:40hs</h6>
+                  <h5>Julio Avantt</h5>
+                  <button type="button">Nueva</button>
+                  <button type="button">Alta</button>
+                  <p>Descripción</p>
+                </div>
               </div>
             </div>
-            <div className="list">
-              <h4>Finalizadas</h4>
-              <div className="card">
-                <div className="close">X</div>
-                <h3>Tarea 1</h3>
-                <h6>24/1/2022 16:40hs</h6>
-                <h5>Julio Avantt</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Alta</button>
-                <p>Descripción</p>
-              </div>
-            </div>
-          </div>
-          <div className="list phone">
-            <div className="card">
-              <div className="close">X</div>
-              <h3>Tarea 1</h3>
-              <h6>24/1/2022 16:40hs</h6>
-              <h5>Julio Avantt</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Alta</button>
-              <p>Descripción</p>
-            </div>
-            <div className="card">
-              <div className="close">X</div>
-              <h3>Tarea 1</h3>
-              <h6>24/1/2022 16:40hs</h6>
-              <h5>Julio Avantt</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Alta</button>
-              <p>Descripción</p>
-            </div>
-            <div className="card">
-              <div className="close">X</div>
-              <h3>Tarea 1</h3>
-              <h6>24/1/2022 16:40hs</h6>
-              <h5>Julio Avantt</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Alta</button>
-              <p>Descripción</p>
-            </div>
-          </div>
+          )}
         </section>
       </main>
     </>
