1ab035e088324becfc2cf9d0c22edd4217419097
mariandotg <marianguillaume.m@gmail.com>
2022-05-16 23:00:14 -0300

    Added simple placeholder List and Card components

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,7 +1,30 @@
+import "./Tasks.styles.css";
 import Header from "../../Header/Header";
 
 const Tasks = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main id="tasks">
+        <section className="wrapper_list">
+          <div className="list_header">
+            <h2>Mis tareas</h2>
+          </div>
+          <div className="list">
+            <div className="card">
+              <div className="close">X</div>
+              <h3>Tarea 1</h3>
+              <h6>24/1/2022 16:40hs</h6>
+              <h5>Julio Avantt</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Alta</button>
+              <p>Descripción</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Tasks;
