2156c2969e8873b71e6031c5c5f1e1ffa60f30a5
mariandotg <marianguillaume.m@gmail.com>
2022-05-19 00:10:21 -0300

    Added basic formik usage

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,23 +1,55 @@
+import { useFormik } from "formik";
 import "./TaskForm.styles.css";
 
 const TaskForm = () => {
+  const initialValues = {
+    title: "",
+    status: "",
+    priority: "",
+    description: "",
+  };
+
+  const onSubmit = () => {
+    alert();
+  };
+
+  const formik = useFormik({ initialValues, onSubmit });
+  const { handleSubmit, handleChange } = formik;
+
   return (
     <section className="task-form">
       <h2>Crear tarea</h2>
       <p>Crea tus tareas</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <div>
-            <input />
+            <input name="title" placeholder="Titulo" />
           </div>
           <div>
-            <select />
+            <select name="status" onChange={handleChange}>
+              <option value="">Seleccionar un estado</option>
+              <option value="new">Nueva</option>
+              <option value="inProcess">En proceso</option>
+              <option value="finished">Terminada</option>
+            </select>
           </div>
           <div>
-            <select />
+            <select name="priority" onChange={handleChange}>
+              <option value="">Seleccionar una prioridad</option>
+              <option value="low">Baja</option>
+              <option value="medium">Media</option>
+              <option value="high">Alta</option>
+            </select>
           </div>
         </div>
-        <div></div>
+        <div>
+          <textarea
+            name="description"
+            onChange={handleChange}
+            placeholder="Descripción"
+          />
+        </div>
+        <button type="submit">Crear</button>
       </form>
     </section>
   );
