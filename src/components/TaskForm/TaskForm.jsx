d290a0a97596251ba330a05a21c7cb5e930ea6e4
mariandotg <marianguillaume.m@gmail.com>
2022-05-19 14:11:36 -0300

    Added Yup validation schema

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,5 +1,6 @@
 import { useFormik } from "formik";
 import "./TaskForm.styles.css";
+import * as Yup from "yup";
 
 const TaskForm = () => {
   const initialValues = {
@@ -9,12 +10,22 @@ const TaskForm = () => {
     description: "",
   };
 
+  const required = "* Campo obligatorio";
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(6, "La cantidad mínima de caracteres es 6")
+      .required(required),
+    status: Yup.string().required(required),
+    priority: Yup.string().required(required),
+  });
+
   const onSubmit = () => {
     alert();
   };
 
-  const formik = useFormik({ initialValues, onSubmit });
-  const { handleSubmit, handleChange } = formik;
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+  const { handleSubmit, handleChange, errors } = formik;
 
   return (
     <section className="task-form">
@@ -23,7 +34,8 @@ const TaskForm = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <div>
-            <input name="title" placeholder="Titulo" />
+            <input name="title" placeholder="Titulo" onChange={handleChange} />
+            {errors.title && <span>{errors.title}</span>}
           </div>
           <div>
             <select name="status" onChange={handleChange}>
@@ -32,6 +44,7 @@ const TaskForm = () => {
               <option value="inProcess">En proceso</option>
               <option value="finished">Terminada</option>
             </select>
+            {errors.status && <span>{errors.status}</span>}
           </div>
           <div>
             <select name="priority" onChange={handleChange}>
@@ -40,6 +53,7 @@ const TaskForm = () => {
               <option value="medium">Media</option>
               <option value="high">Alta</option>
             </select>
+            {errors.priority && <span>{errors.priority}</span>}
           </div>
         </div>
         <div>
