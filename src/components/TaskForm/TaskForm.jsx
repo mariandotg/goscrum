d0971e2116b98b71c3552212e892dde57dbfbc35
mariandotg <marianguillaume.m@gmail.com>
2022-05-30 00:38:14 -0300

    Add task function

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -2,11 +2,13 @@ import { useFormik } from "formik";
 import "./TaskForm.styles.css";
 import * as Yup from "yup";
 
+const { REACT_APP_API_ENDPOINT } = process.env;
+
 const TaskForm = () => {
   const initialValues = {
     title: "",
     status: "",
-    priority: "",
+    importance: "",
     description: "",
   };
 
@@ -17,15 +19,36 @@ const TaskForm = () => {
       .min(6, "La cantidad mínima de caracteres es 6")
       .required(required),
     status: Yup.string().required(required),
-    priority: Yup.string().required(required),
+    importance: Yup.string().required(required),
+    description: Yup.string().required(required),
   });
 
   const onSubmit = () => {
-    alert();
+    fetch(`${REACT_APP_API_ENDPOINT}/task`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({ task: values }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resetForm();
+        alert("tu tarea se creó");
+      });
   };
 
   const formik = useFormik({ initialValues, validationSchema, onSubmit });
-  const { handleSubmit, handleChange, handleBlur, errors, touched } = formik;
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors,
+    touched,
+    values,
+    resetForm,
+  } = formik;
 
   return (
     <section className="task-form">
@@ -39,7 +62,8 @@ const TaskForm = () => {
               placeholder="Titulo"
               onChange={handleChange}
               onBlur={handleBlur}
-              className={errors.title ? "error" : ""}
+              className={errors.title && touched.title ? "error" : ""}
+              value={values.title}
             />
             {errors.title && touched.title && (
               <span className="error-message">{errors.title}</span>
@@ -50,12 +74,13 @@ const TaskForm = () => {
               name="status"
               onChange={handleChange}
               onBlur={handleBlur}
-              className={errors.status ? "error" : ""}
+              className={errors.status && touched.status ? "error" : ""}
+              value={values.status}
             >
               <option value="">Seleccionar un estado</option>
-              <option value="new">Nueva</option>
-              <option value="inProcess">En proceso</option>
-              <option value="finished">Terminada</option>
+              <option value="NEW">Nueva</option>
+              <option value="IN PROGRESS">En proceso</option>
+              <option value="FINISHED">Terminada</option>
             </select>
             {errors.status && touched.status && (
               <span className="error-message">{errors.status}</span>
@@ -63,18 +88,19 @@ const TaskForm = () => {
           </div>
           <div>
             <select
-              name="priority"
+              name="importance"
               onChange={handleChange}
               onBlur={handleBlur}
-              className={errors.priority ? "error" : ""}
+              className={errors.importance && touched.importance ? "error" : ""}
+              value={values.importance}
             >
               <option value="">Seleccionar una prioridad</option>
-              <option value="low">Baja</option>
-              <option value="medium">Media</option>
-              <option value="high">Alta</option>
+              <option value="LOW">Baja</option>
+              <option value="MEDIUM">Media</option>
+              <option value="HIGH">Alta</option>
             </select>
-            {errors.priority && touched.priority && (
-              <span className="error-message">{errors.priority}</span>
+            {errors.importance && touched.importance && (
+              <span className="error-message">{errors.importance}</span>
             )}
           </div>
         </div>
@@ -83,8 +109,13 @@ const TaskForm = () => {
             name="description"
             onChange={handleChange}
             onBlur={handleBlur}
+            className={errors.description && touched.description ? "error" : ""}
             placeholder="Descripción"
+            value={values.description}
           />
+          {errors.description && touched.description && (
+            <span className="error-message">{errors.description}</span>
+          )}
         </div>
         <button type="submit">Crear</button>
       </form>
