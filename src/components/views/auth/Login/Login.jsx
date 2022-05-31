039fff6179f236eb9133518a6b2be7e9a5d01b23
mariandotg <marianguillaume.m@gmail.com>
2022-05-31 18:03:43 -0300

    Added simple sweet alert component

--- src/components/views/auth/Login/Login.jsx
+++ src/components/views/auth/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useFormik } from "formik";
 import { useNavigate, Link } from "react-router-dom";
 import * as Yup from "yup";
 import "../Auth.styles.css";
+import swal from "../../../../utills/swal";
 
 const { REACT_APP_API_ENDPOINT } = process.env;
 
@@ -40,7 +41,7 @@ const Login = () => {
           localStorage.setItem("token", data?.result?.token);
           navigate(`/`, { replace: true });
         } else {
-          alert("Malas credenciales");
+          swal();
         }
       });
   };
