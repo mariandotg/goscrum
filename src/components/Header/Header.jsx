68ecbfb9d52a090b2cce13fab8c60ecff09ac8a9
mariandotg <marianguillaume.m@gmail.com>
2022-06-01 13:24:11 -0300

    Added userName on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,17 @@ const Header = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("userName");
     navigate("/", { replace: true });
   };
 
   return (
     <header>
       <img src="/img/goscrum.png" alt="logo" />
-      <div onClick={handleLogout}>X</div>
+      <div className="wrapper_right_header">
+        <div>{localStorage.getItem("userName")}</div>
+        <div onClick={handleLogout}>X</div>
+      </div>
     </header>
   );
 };
--- src/components/views/auth/Login/Login.jsx
+++ src/components/views/auth/Login/Login.jsx
@@ -39,6 +39,7 @@ const Login = () => {
       .then((data) => {
         if (data.status_code === 200) {
           localStorage.setItem("token", data?.result?.token);
+          localStorage.setItem("userName", data?.result?.user?.userName);
           navigate(`/`, { replace: true });
         } else {
           swal();
