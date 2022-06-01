b739818eb96eb72095248f325bfabedb54fa322d
Mariano Guillaume <84543990+mariandotg@users.noreply.github.com>
2022-06-01 13:11:40 -0300

    Tasks (#14)
    * Added limitString function to the Card component
    
    * Added functions for dynamic render
    
    * Added skeleton

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,28 @@
 const Card = ({
-  data: { title, datetime, creator, description, type, priority },
+  data: {
+    title,
+    createdAt,
+    user: { userName },
+    description,
+    status,
+    importance,
+  },
 }) => {
+  const limitString = (string) => {
+    if (string.length > 150) {
+      return { string: string.slice(0, 147).concat("..."), addButton: true };
+    }
+    return { string, addButton: false };
+  };
   return (
     <div className="card">
       <div className="close">X</div>
       <h3>{title}</h3>
-      <h6>{datetime}</h6>
-      <h5>{creator}</h5>
-      <button type="button">{type}</button>
-      <button type="button">{priority}</button>
-      <p>{description}</p>
+      <h6>{createdAt}</h6>
+      <h5>{userName}</h5>
+      <button type="button">{status}</button>
+      <button type="button">{importance}</button>
+      <p>{limitString(description).string}</p>
     </div>
   );
 };
--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,22 +1,56 @@
-import { cardsData } from "./data";
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 import { useResize } from "../../../hooks/useResize";
+import "react-loading-skeleton/dist/skeleton.css";
 import "./Tasks.styles.css";
 import Header from "../../Header/Header";
 import Card from "../../Card/Card";
 import TaskForm from "../../TaskForm/TaskForm";
 
+const { REACT_APP_API_ENDPOINT } = process.env;
+
 const Tasks = () => {
+  const [list, setList] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { isPhone } = useResize();
 
-  const limitString = (string) => {
-    if (string.length > 370) {
-      return { string: string.slice(0, 367).concat("..."), addButton: true };
-    }
-    return { string, addButton: false };
-  };
+  useEffect(() => {
+    setLoading(true);
+    fetch(`${REACT_APP_API_ENDPOINT}/task`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setList(data.result);
+        setTimeout(() => {
+          setLoading(false);
+        }, 4000);
+      });
+  }, []);
 
   const renderAllCards = () => {
-    return cardsData.map((data) => <Card key={data.id} data={data} />);
+    return list?.map((data) => <Card key={data._id} data={data} />);
+  };
+
+  const renderNewCards = () => {
+    return list
+      ?.filter((data) => data.status === "NEW")
+      .map((data) => <Card key={data._id} data={data} />);
+  };
+
+  const renderInProgressCards = () => {
+    return list
+      ?.filter((data) => data.status === "IN PROGRESS")
+      .map((data) => <Card key={data._id} data={data} />);
+  };
+
+  const renderFinishedCards = () => {
+    return list
+      ?.filter((data) => data.status === "FINISHED")
+      .map((data) => <Card key={data._id} data={data} />);
   };
 
   return (
@@ -29,45 +63,43 @@ const Tasks = () => {
             <h2>Mis tareas</h2>
           </div>
           {isPhone ? (
-            <div className="list phone">{renderAllCards()}</div>
+            !list?.length ? (
+              <div>No hay tareas creadas</div>
+            ) : loading ? (
+              <>
+                <Skeleton height={90} />
+                <Skeleton height={90} />
+                <Skeleton height={90} />
+              </>
+            ) : (
+              <div className="list phone">{renderAllCards()}</div>
+            )
           ) : (
             <div className="list_group">
-              <div className="list">
-                <h4>Nuevas</h4>
-                <div className="card">
-                  <div className="close">X</div>
-                  <h3>Tarea 1</h3>
-                  <h6>24/1/2022 16:40hs</h6>
-                  <h5>Julio Avantt</h5>
-                  <button type="button">Nueva</button>
-                  <button type="button">Alta</button>
-                  <p>Descripción</p>
-                </div>
-              </div>
-              <div className="list">
-                <h4>En proceso</h4>
-                <div className="card">
-                  <div className="close">X</div>
-                  <h3>Tarea 1</h3>
-                  <h6>24/1/2022 16:40hs</h6>
-                  <h5>Julio Avantt</h5>
-                  <button type="button">Nueva</button>
-                  <button type="button">Alta</button>
-                  <p>Descripción</p>
-                </div>
-              </div>
-              <div className="list">
-                <h4>Finalizadas</h4>
-                <div className="card">
-                  <div className="close">X</div>
-                  <h3>Tarea 1</h3>
-                  <h6>24/1/2022 16:40hs</h6>
-                  <h5>Julio Avantt</h5>
-                  <button type="button">Nueva</button>
-                  <button type="button">Alta</button>
-                  <p>Descripción</p>
-                </div>
-              </div>
+              {!list?.length ? (
+                <div>No hay tareas creadas</div>
+              ) : loading ? (
+                <>
+                  <Skeleton height={90} />
+                  <Skeleton height={90} />
+                  <Skeleton height={90} />
+                </>
+              ) : (
+                <>
+                  <div className="list">
+                    <h4>Nuevas</h4>
+                    {renderNewCards()}
+                  </div>
+                  <div className="list">
+                    <h4>En proceso</h4>
+                    {renderInProgressCards()}
+                  </div>
+                  <div className="list">
+                    <h4>Finalizadas</h4>
+                    {renderFinishedCards()}
+                  </div>
+                </>
+              )}
             </div>
           )}
         </section>
