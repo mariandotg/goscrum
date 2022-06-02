548577f24ad116a33e3b924faf55de11726dad81
mariandotg <marianguillaume.m@gmail.com>
2022-06-02 00:11:40 -0300

    Added show more button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const Card = ({
   data: {
     title,
@@ -8,21 +9,42 @@ const Card = ({
     importance,
   },
 }) => {
+  const [showMore, setShowMore] = useState(false);
+  const dateTime = new Date(createdAt).toLocaleString() + " hs.";
+
   const limitString = (string) => {
-    if (string.length > 150) {
-      return { string: string.slice(0, 147).concat("..."), addButton: true };
+    if (string.length > 170) {
+      return { string: string.slice(0, 167).concat("..."), addButton: true };
     }
     return { string, addButton: false };
   };
+
   return (
     <div className="card">
       <div className="close">X</div>
       <h3>{title}</h3>
-      <h6>{createdAt}</h6>
+      <h6>{dateTime}</h6>
       <h5>{userName}</h5>
-      <button type="button">{status}</button>
-      <button type="button">{importance}</button>
-      <p>{limitString(description).string}</p>
+      <button className={status.toLowerCase()} type="button">
+        {status}
+      </button>
+      <button className={importance.toLowerCase()} type="button">
+        {importance}
+      </button>
+      {!showMore && <p>{limitString(description).string}</p>}
+      {showMore && (
+        <>
+          <p>{description}</p>
+          <button type="button" onClick={() => setShowMore(false)}>
+            Ver menos
+          </button>
+        </>
+      )}
+      {!showMore && limitString(description).addButton && (
+        <button type="button" onClick={() => setShowMore(true)}>
+          Ver más
+        </button>
+      )}
     </div>
   );
 };
