abb79fb878a80295530d61b6e4aa4b32e9d48e8f
Mariano Guillaume <84543990+mariandotg@users.noreply.github.com>
2022-06-02 00:22:46 -0300

    Tasks (#16)
    * Added functions for dynamic render
    
    * Added skeleton
    
    * Added tasks filter by userName
    
    * Added searchbar for tasks
    
    * Added show more button

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
-import { useResize } from "../../../hooks/useResize";
+import debounce from "lodash.debounce";
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import "react-loading-skeleton/dist/skeleton.css";
 import "./Tasks.styles.css";
+import { useResize } from "../../../hooks/useResize";
 import Header from "../../Header/Header";
 import Card from "../../Card/Card";
 import TaskForm from "../../TaskForm/TaskForm";
@@ -10,48 +17,63 @@ import TaskForm from "../../TaskForm/TaskForm";
 const { REACT_APP_API_ENDPOINT } = process.env;
 
 const Tasks = () => {
+  const [tasksFromWho, setTasksFromWho] = useState("ALL");
   const [list, setList] = useState(null);
+  const [renderList, setRenderList] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
   const { isPhone } = useResize();
 
   useEffect(() => {
     setLoading(true);
-    fetch(`${REACT_APP_API_ENDPOINT}/task`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    })
+    fetch(
+      `${REACT_APP_API_ENDPOINT}/task${tasksFromWho === "ME" ? "/me" : ""}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         setList(data.result);
+        setRenderList(data.result);
         setTimeout(() => {
           setLoading(false);
         }, 4000);
       });
-  }, []);
+  }, [tasksFromWho]);
+
+  useEffect(() => {
+    if (search) {
+      setRenderList(list.filter((data) => data.title.startsWith(search)));
+    } else {
+      setRenderList(list);
+    }
+  }, [search]);
 
   const renderAllCards = () => {
-    return list?.map((data) => <Card key={data._id} data={data} />);
+    return renderList?.map((data) => <Card key={data._id} data={data} />);
   };
 
-  const renderNewCards = () => {
-    return list
-      ?.filter((data) => data.status === "NEW")
+  const renderColumnCards = (text) => {
+    return renderList
+      ?.filter((data) => data.status === text)
       .map((data) => <Card key={data._id} data={data} />);
   };
 
-  const renderInProgressCards = () => {
-    return list
-      ?.filter((data) => data.status === "IN PROGRESS")
-      .map((data) => <Card key={data._id} data={data} />);
+  const handleChangeImportance = (event) => {
+    if (event.currentTarget.value === "ALL") setRenderList(list);
+    else
+      setRenderList(
+        list.filter((data) => data.importance === event.currentTarget.value)
+      );
   };
 
-  const renderFinishedCards = () => {
-    return list
-      ?.filter((data) => data.status === "FINISHED")
-      .map((data) => <Card key={data._id} data={data} />);
-  };
+  const handleSearch = debounce((event) => {
+    setSearch(event?.target?.value);
+  }, 1000);
 
   return (
     <>
@@ -62,8 +84,42 @@ const Tasks = () => {
           <div className="list_header">
             <h2>Mis tareas</h2>
           </div>
+          <div className="filters">
+            <FormControl>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                onChange={(event) => setTasksFromWho(event.currentTarget.value)}
+              >
+                <FormControlLabel
+                  value="ALL"
+                  control={<Radio />}
+                  label="Todas"
+                />
+                <FormControlLabel
+                  value="ME"
+                  control={<Radio />}
+                  label="Mis tareas"
+                />
+              </RadioGroup>
+            </FormControl>
+            <div className="search">
+              <input
+                type="text"
+                placeholder="Buscar por titulo..."
+                onChange={handleSearch}
+              />
+            </div>
+            <select name="importance" onChange={handleChangeImportance}>
+              <option value="">Seleccionar una prioridad</option>
+              <option value="ALL">Todas</option>
+              <option value="LOW">Baja</option>
+              <option value="MEDIUM">Media</option>
+              <option value="HIGH">Alta</option>
+            </select>
+          </div>
           {isPhone ? (
-            !list?.length ? (
+            !renderList?.length ? (
               <div>No hay tareas creadas</div>
             ) : loading ? (
               <>
@@ -76,7 +132,7 @@ const Tasks = () => {
             )
           ) : (
             <div className="list_group">
-              {!list?.length ? (
+              {!renderList?.length ? (
                 <div>No hay tareas creadas</div>
               ) : loading ? (
                 <>
@@ -88,15 +144,15 @@ const Tasks = () => {
                 <>
                   <div className="list">
                     <h4>Nuevas</h4>
-                    {renderNewCards()}
+                    {renderColumnCards("NEW")}
                   </div>
                   <div className="list">
                     <h4>En proceso</h4>
-                    {renderInProgressCards()}
+                    {renderColumnCards("IN PROGRESS")}
                   </div>
                   <div className="list">
                     <h4>Finalizadas</h4>
-                    {renderFinishedCards()}
+                    {renderColumnCards("FINISHED")}
                   </div>
                 </>
               )}
